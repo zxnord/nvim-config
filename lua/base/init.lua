e4d18677459063dae0bd8dfddcb4481df9b17cf2
Edson Contreras <edrecon@gmail.com>
2024-12-27 19:11:24 -0300

    my very first commit

---
+++ lua/base/init.lua
@@ -0,0 +1,14 @@
+-- Base settings
+
+vim.g.mapleader = " "
+
+vim.wo.relativenumber = true
+vim.wo.number = true
+-- Mappings
+vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>1", vim.cmd.bfirst)
+vim.keymap.set("n", "<leader>0", vim.cmd.blast)
+vim.keymap.set("n", "<Tab>", vim.cmd.bnext)
+vim.keymap.set("n", "<S-Tab>", vim.cmd.bprevious)
+
+require("base.plugins.lazy")
---
+++ lua/configs/mason.lua
@@ -0,0 +1,20 @@
+return {
+	ensure_installed = {
+		"lua-language-server",
+		"stylua",
+		"rust-analyzer",
+		"pyright",
+		"clangd",
+		"prettier",
+		"black",
+		"isort",
+		"typescript-language-server",
+		"svelte-language-server",
+		"codelldb",
+		"debugpy",
+		"zls",
+		"astro-language-server",
+	},
+
+	max_concurrent_installers = 10,
+}
